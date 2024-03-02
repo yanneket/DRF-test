480b71c58e0762f46637740d1d98c22b9ac52886
Nikita Yanchenko <maraabus@mail.ru>
2024-03-02 23:05:12 +0700

    create class Serializer

--- DRF/women/serializers.py
+++ DRF/women/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ("title", "cat_id")
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
