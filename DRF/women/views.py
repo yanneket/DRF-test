a0e5fe34f95bb7bade5136a3eed66b46b69150f1
Nikita Yanchenko <maraabus@mail.ru>
2024-03-04 13:57:02 +0700

    Viewsets and ModelViewSet

--- DRF/women/views.py
+++ DRF/women/views.py
@@ -5,13 +5,19 @@ from .serializers import WomenSerializer
 from .models import Women
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-
-class WomenAPIList(generics. ListCreateAPIView):
+from rest_framework import viewsets
+    
+    
+class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
 
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer  
+# class WomenAPIList(generics. ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer  
